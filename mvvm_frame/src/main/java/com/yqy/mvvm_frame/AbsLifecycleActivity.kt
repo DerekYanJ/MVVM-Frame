6e0d4a304e6f0eec30f3b5e1171fea3cd054f3fb
闫秋阳 <yanjiuyangok@163.com>
2019-05-16 14:22:46 +0800

    Add MVVM code

---
+++ mvvm_frame/src/main/java/com/yqy/mvvm_frame/AbsLifecycleActivity.kt
@@ -0,0 +1,44 @@
+package com.yqy.mvvm_frame
+
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProviders
+
+/**
+ * @desc
+ * @author derekyan
+ * @date 2019-05-13
+ */
+abstract class AbsLifecycleActivity<T : BaseViewModel<*>>: BaseActivity() {
+
+    protected var mViewModel: T? = null
+
+
+    override fun initView() {
+        mViewModel = VMProviders(this, TUtil.getInstance(this, 0));
+        if (null != mViewModel) {
+            dataObserver()
+        }
+    }
+
+
+    protected fun dataObserver() {
+
+    }
+
+    /**
+     * 获取网络数据
+     */
+    protected fun getRemoteData() {
+
+    }
+
+    /**
+     * create ViewModelProviders
+     *
+     * @return ViewModel
+     */
+    protected fun <T : ViewModel> VMProviders(activity: BaseActivity, modelClass: Class<T>): T {
+        return ViewModelProviders.of(activity).get(modelClass)
+    }
+}
